040bfd267859e5f35bc0165f7021eb6ac03cd855
Ümit Arik <umtark3@gmail.com>
2025-07-01 21:23:16 +0300

    ytmp3_server.py

--- ytmp3_server.py
+++ ytmp3_server.py
@@ -70,6 +70,6 @@ def mp3():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     print(f"[BİLGİ] Uygulama başlatılıyor, port: {port}")
     app.run(host='0.0.0.0', port=port)
