93e3244995c839ed86c912bd96d398c24ff34f9f
Ümit Arik <umtark3@gmail.com>
2025-07-01 18:19:25 +0300

    ytmp3_server.py

--- ytmp3_server.py
+++ ytmp3_server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, send_file, abort
-import yt_dlp
+import base64
 import tempfile
 import os
+from flask import Flask, request, send_file, abort
+import yt_dlp
 
 app = Flask(__name__)
 
@@ -13,6 +14,13 @@ def mp3():
 
     try:
         with tempfile.TemporaryDirectory() as tmp:
+            cookie_path = None
+            cookies_b64 = os.environ.get('COOKIES_JSON')
+            if cookies_b64:
+                cookie_path = os.path.join(tmp, 'cookies.txt')
+                with open(cookie_path, 'wb') as f:
+                    f.write(base64.b64decode(cookies_b64))
+
             ydl_opts = {
                 'format': 'bestaudio/best',
                 'outtmpl': os.path.join(tmp, '%(title)s.%(ext)s'),
@@ -28,11 +36,15 @@ def mp3():
                 'nooverwrites': True,
                 'noplaylist': True,
             }
+            if cookie_path:
+                ydl_opts['cookiefile'] = cookie_path
+
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
                 if info is None:
                     return "Video indirilemedi veya desteklenmeyen URL", 400
                 filename = ydl.prepare_filename(info).rsplit('.', 1)[0] + '.mp3'
+
             if not os.path.exists(filename):
                 return "Dosya oluşturulamadı", 500
             return send_file(filename, as_attachment=True)
