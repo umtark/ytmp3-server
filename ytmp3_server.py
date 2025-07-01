438d19d60cbb1aba87ab6610bac1e6333585c748
Ümit Arık <umtark3@gmail.com>
2025-07-01 10:25:31 +0300

    İlk yükleme: MP3 sunucu dosyaları

---
+++ ytmp3_server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, send_file
+import yt_dlp
+import tempfile
+import os
+
+app = Flask(__name__)
+
+@app.route('/mp3')
+def mp3():
+    url = request.args.get('url')
+    if not url:
+        return "URL parametresi eksik", 400
+
+    with tempfile.TemporaryDirectory() as tmp:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(tmp, '%(title)s.%(ext)s'),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info).rsplit('.', 1)[0] + '.mp3'
+        return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
